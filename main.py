e0aeeb54b887a1eef6e842836d1c5d55bb28e92b
mdmarti <miles.martinez@duke.edu>
2025-11-16 18:52:14 -0500

    get rid of vis stuff

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ log_root = train_config['log_root']
 log_path, log_dir = init_log(log_root, train_config)
 print('Experiment: ' + log_dir)
 
-global vis
-vis, handle_dict = init_plot(train_config, arch, env=log_dir)
+#global vis
+#vis, handle_dict = init_plot(train_config, arch, env=log_dir)
 
 # load data, labels
 data_path = train_config['data_path']
