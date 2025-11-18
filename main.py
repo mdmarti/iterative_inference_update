6827bc91a265853741185ecb7494700389db37b5
mdmarti <miles.martinez@duke.edu>
2025-11-17 19:02:41 -0500

    oopth

--- main.py
+++ main.py
@@ -67,9 +67,9 @@ for n_iterations in [1,5,50,100,200,300]:
         tic = time.time()
         model.train()
         train_dict = train(model, train_config, arch, train_loader, epoch+1, (enc_opt, dec_opt))
-        iter_dict['elbos']['train'].append(train_dict['total_elbo'][:,-1])
-        iter_dict['log_probs']['train'].append(train_dict['total_cond_log_like'][:,-1])
-        iter_dict['kls']['train'].append(train_dict['total_kl'][0][:,-1])
+        iter_dict['elbos']['train'].append(train_dict['avg_elbo'])
+        iter_dict['log_probs']['train'].append(train_dict['avg_cond_like'])
+        iter_dict['kls']['train'].append(train_dict['avg_kl'][-1])
         toc = time.time()
         print('Training Time: ' + str(toc - tic))
         # validation
